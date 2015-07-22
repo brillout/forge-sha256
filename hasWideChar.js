4cb1b27c9d2be3353126b37685fcc615ac765ee9
brillout <brillout@users.noreply.github.com>
2015-07-22 13:59:05 +0200

    improve wording

--- hasWideChar.js
+++ hasWideChar.js
@@ -1,4 +1,4 @@
-/* custom written function to determine if string is ASCII or UTF-8 */
+/* custom written function to determine if string is composed of only ASCII characters */
 util.hasWideChar = function(str) {
     for( var i = 0; i < str.length; i++ ){
         if ( str.charCodeAt(i) >>> 8 ) return true;
